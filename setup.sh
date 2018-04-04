cab0281ba916dd99b233ea8487b3590a4b1942df
LinxiFan <jimfanspire@gmail.com>
2018-04-04 11:05:36 -0700

    source bashrc

--- setup.sh
+++ setup.sh
@@ -5,8 +5,12 @@
 cat $1/bashrc_patch.sh >> ~/.bashrc
 echo '~/.bashrc patched'
 
+source ~/.bashrc
+echo '~/.bashrc reloaded'
+
 mkdir -p ~/.jupyter
 cp -f $1/jupyter_notebook_config.py ~/.jupyter
 echo 'jupyter_notebook_config.py copied to ~/.jupyter'
 
 echo 'Setup complete. Enjoy!'
+
