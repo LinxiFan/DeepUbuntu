0de87fdf5bfed2458f522f14ad62eea88b276463
LinxiFan <jimfanspire@gmail.com>
2018-04-04 11:04:47 -0700

    Add more messages

--- setup.sh
+++ setup.sh
@@ -3,7 +3,10 @@
 # usage: ./setup.sh <git-repo-folder>
 
 cat $1/bashrc_patch.sh >> ~/.bashrc
-echo '~/.bashrc successfully patched'
+echo '~/.bashrc patched'
 
 mkdir -p ~/.jupyter
 cp -f $1/jupyter_notebook_config.py ~/.jupyter
+echo 'jupyter_notebook_config.py copied to ~/.jupyter'
+
+echo 'Setup complete. Enjoy!'
